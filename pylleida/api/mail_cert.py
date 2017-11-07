347163207dbcbe24dc78c210c250dc185a87bc93
Nicolas Jaremek <nick13jaremek@gmail.com>
2017-11-07 18:10:16 +0100

    Implemented 'list_pdf' method of MailCert API.

--- pylleida/api/mail_cert.py
+++ pylleida/api/mail_cert.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+from pylleida.api import BaseApi, BaseResponse
 
-from pylleida.api import BaseApi
+
+class MailCertApiResponse(BaseResponse):
+    pass
 
 
 class MailCertApi(BaseApi):
@@ -12,8 +16,44 @@ class MailCertApi(BaseApi):
         params = {}
         return self.post(endpoint=self.endpoint, template_name=template_name, in_params=params)
 
-    def list_pdf(self):
-        pass
+    def list_pdf(self,
+                 mail_from=None, mail_to=None,
+                 date_min=None, date_max=None,
+                 file_id_min=None, file_id_max=None,
+                 file_type=None, subjrw=None,
+                 mail_message_id=None, row_order=None,
+                 only_last_file_id=False):
+        """
+        Returns a list of items, each item referencing a Lleida.net email certificante.
+        The input parameters act as search criteria to narrow the returned results.
+
+        :param mail_from: results by a given sender's email address.
+        :param mail_to: filter results by a given recipient's email address.
+        :param date_min: filter results starting from an initial date.
+        :param date_max: filter results starting from a final date.
+        :param file_id_min: filter results starting from a minimum file_id.
+        :param file_id_max: filter results up to a maximum file_id.
+        :param file_type: filter results by the file/document type.
+        :param subjrw: filter results based on a substring that appears in the emails' subjects. It is advised to
+        use ASCII alphanumeric values that may appear in the first 200 character positions of the subject.
+        :param mail_message_id: filter results by the message id of an email certificate.
+        :param row_order: filter results by ascending or descending order based on the email certificate id.
+        Valid values are 'asc' for ascending order, 'desc' for descending order.
+        :param only_last_file_id: return a single element, (the latest email certificate file identifier)
+        in the response.
+        :return: a JSON response containing the results.
+        """
+        template_name = 'list_pdf.xml'
+        params = {
+            'mail_from': mail_from, 'mail_to': mail_to,
+            'date_min': date_min, 'date_max': date_max,
+            'file_id_min': file_id_min, 'file_id_max': file_id_max,
+            'file_type': file_type, 'subjrw': subjrw,
+            'mail_message_id': mail_message_id, 'row_order': row_order,
+            'only_last_file_id': only_last_file_id
+        }
+        response = self.post(endpoint=self.endpoint, template_name=template_name, in_params=params)
+        return MailCertApiResponse(response['result']['pdf_list'])
 
     def download_pdf(self):
         pass
