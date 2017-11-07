3a7773bd0bfe0060ef2a1d38f691beb19c37ffd6
Nicolas Jaremek <nick13jaremek@gmail.com>
2017-11-07 11:37:33 +0100

    Initial implementation of the PyLleida client class.

--- pylleida/__init__.py
+++ pylleida/__init__.py
@@ -2,6 +2,28 @@
 
 """Top-level package for pylleida."""
 
-__author__ = """Nick M. Jaremek"""
+__author__ = """NickM. Jaremek"""
 __email__ = 'nick13jaremek@gmail.com'
 __version__ = '0.1.0'
+
+from pylleida.api.mail_cert import MailCertApi
+
+
+class PyLleida(object):
+    def __init__(self, username, password):
+        """
+        Python wrapper for the Lleida.net API.
+
+        There exists only one way to authenticate with the Lleida.net API, which consists on providing a
+        username/password combination.
+
+        :param username: Lleida.net username
+        :param password: Lleida.net password
+        """
+
+        config = dict(
+            username=username,
+            password=password
+        )
+
+        self.mailcert = MailCertApi(config)
