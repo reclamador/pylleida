3ef6f9f072f80dd45be997c9241dcca1f727ebc2
Nicolas Jaremek <nick13jaremek@gmail.com>
2017-11-07 18:09:55 +0100

    Added BaseResponse and ResponseItem classes. BaseResponse is in charge of converting a dict object (a HTTP response data) into an object whose attributes are the HTTP request response data accessible via dot syntax. Also, HTTP response data fields prefixed with an @ character (due to XML-to-JSON conversion) are normalized by removing the @ character.

--- pylleida/api/__init__.py
+++ pylleida/api/__init__.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 import json
 import requests
 import xmltodict
@@ -13,7 +13,7 @@ class BaseApi(object):
         self.password = password
 
     def _xml_to_json(self, xml_content):
-        return json.dumps(xml_content)['result']  # XML response data are wrapped in 'result' tag
+        return json.loads(json.dumps(xml_content))
 
     def _updated_params(self, in_params):
         base_params = {'username': self.username, 'password': self.password}
@@ -24,10 +24,44 @@ class BaseApi(object):
         template = render_template(template_filename=template_name, **params)
 
         request_data = {'xml': template}
-        headers = {'content-type': 'application/soap+xml;charset=utf-8', 'SOAPAction': 'any'}
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
 
         response = requests.post(endpoint, data=request_data, headers=headers, timeout=self.TIMEOUT)
         xml_response = xmltodict.parse(xml_input=response.content)
         return self._xml_to_json(xml_response)
 
+
+class ResponseItem(object):
+    pass
+
+
+class BaseResponse(object):
+    def __init__(self, json_data):
+        self._update_attributes(self, json_data)
+
+    def _format_key(self, key):
+        if key.startswith('@'):
+            return key.replace('@', '')
+        return key
+
+    def _update_attributes(self, obj, data):
+        for key in data.keys():
+            if key.startswith('_'):
+                continue
+
+            formatted_key = self._format_key(key)
+            value = data[key]
+            if isinstance(value, list):
+                value_items = []
+                for item in value:
+                    new_obj = ResponseItem()
+                    self._update_attributes(new_obj, item)
+                    value_items.append(new_obj)
+                setattr(obj, formatted_key, value_items)
+            elif isinstance(value, dict):
+                new_obj = ResponseItem()
+                self._update_attributes(new_obj, value)
+                setattr(obj, formatted_key, new_obj)
+            else:
+                setattr(obj, formatted_key, value)
 
