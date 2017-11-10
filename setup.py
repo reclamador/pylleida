d134bcd90e406746dbae8e4afbd4aa91101cc5fd
Nicolas Jaremek <nick13jaremek@gmail.com>
2017-11-10 10:15:33 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='pylleida',
-    version='0.2.0',
+    version='0.2.1',
     description="A HTTP Python client to use the Lleida.net API",
     long_description=readme + '\n\n' + history,
     author="Nick M. Jaremek",
