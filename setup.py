2a0f7cb4c67c5eac85e74d618871a979e29eb43c
Juan Madurga <jlmadurga@gmail.com>
2019-10-11 12:01:03 +0200

    fix setup.py classifiers

--- setup.py
+++ setup.py
@@ -39,9 +39,6 @@ setup(
     zip_safe=False,
     keywords='pylleida',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7'
